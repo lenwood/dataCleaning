709a6723874c7b39219ea41a2812dc08beb343ab
Chris Leonard <lenwood.cc@gmail.com>
2018-08-29 16:20:22 -0500

    updated with pandas native functions

--- Python/notInBoth.py
+++ Python/notInBoth.py
@@ -10,6 +10,15 @@ DF2 = pd.DataFrame(
      'BB' : ['D', 'E', 'H', 'I', 'J', 'K', 'L']
     })
 
+# method 1 uses the pandas isin function
+not_in_DF1_method1 = DF2[~DF2['AA'].isin(DF1['AA'])]
+print(not_in_DF1_method1)
+
+
+not_in_DF2_method1 = DF1[~DF1['AA'].isin(DF2['AA'])]
+print(not_in_DF2_method1)
+
+# method 2 is more generic but produces the same results
 DF1list = [True] * DF1.shape[0]
 DF2list = [True] * DF2.shape[0]
 
@@ -18,14 +27,14 @@ DF2.loc[:, 'inDF2'] = DF2list
 
 bigDF = pd.merge(DF1, DF2, how="outer")
 
-not_in_DF1 = bigDF.drop('inDF2', axis=1)
-not_in_DF1 = not_in_DF1[pd.isnull(not_in_DF1).any(axis=1)]
-not_in_DF1 = not_in_DF1.drop('inDF1', axis=1)
+not_in_DF1_method2 = bigDF.drop('inDF2', axis=1)
+not_in_DF1_method2 = not_in_DF1_method2[pd.isnull(not_in_DF1_method2).any(axis=1)]
+not_in_DF1_method2 = not_in_DF1_method2.drop('inDF1', axis=1)
 
-print not_in_DF1
+print(not_in_DF1_method2)
 
-not_in_DF2 = bigDF.drop('inDF1', axis=1)
-not_in_DF2 = not_in_DF2[pd.isnull(not_in_DF2).any(axis=1)]
-not_in_DF2 = not_in_DF2.drop('inDF2', axis=1)
+not_in_DF2_method2 = bigDF.drop('inDF1', axis=1)
+not_in_DF2_method2 = not_in_DF2_method2[pd.isnull(not_in_DF2_method2).any(axis=1)]
+not_in_DF2_method2 = not_in_DF2_method2.drop('inDF2', axis=1)
 
-print not_in_DF2
+print(not_in_DF2_method2)
