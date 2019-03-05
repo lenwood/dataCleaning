038017e729ab8534e31be40a216354ec3b7b7eae
Chris Leonard <lenwood.cc@gmail.com>
2019-03-05 14:46:27 -0600

    no more warning messages
    corrected rows that were generating warnings

--- Python/categorizeDF.py
+++ Python/categorizeDF.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 3.6
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 def typeChanger(s):
     if isinstance(s, (bytes, bytearray)):
@@ -18,47 +18,40 @@ def typeChanger(s):
         newS = s
     return newS
 
-search = ["chicken", "pepperoni", "egg", "cheeseburger", "blt", "omlette"]
+def categorizeDF(df, searchColName, searchList, catList, newColName="Category", undefinedValue="Undefined"):
+    # create copy of df to avoid altering the orginal
+    myDF = df.copy()
 
-category = ["Chicken", "Beef", "Egg", "Beef", "Pork", "Egg"]
-
-food = pd.DataFrame(
-    {'dish' : ["chicken cordon bleu", b"Pepperoni Pizza", "", b"egg salad", "kaese spaetzle", "bagel", "GOULASH", b"Fried Chicken", "Chili Relleno", "cheeseburger", np.nan, "BLT", "omlette"],
-     'meal' : ["dinner", "dinner", "", "lunch", np.nan, "breakfast", "dinner", "lunch", "dinner", "lunch", np.nan, "lunch", "breakfast"] 
-    })
-
-def categorizeDF(df, searchColName, searchList, catList, newColName="Category", undefined="Undefined"):
     # Add default category to all rows to begin with
     # this will be the value for unmatched rows
-    oList = [undefined] * df.shape[0]
-    df.loc[:, newColName] = oList
+    myDF.loc[:, newColName] = undefinedValue
 
     # Add ID so that original sequence can be restored
-    df.loc[:, 'ID'] = list(range(1, df.shape[0]+1)) # +1 to account for header row
+    myDF.loc[:, 'ID'] = list(range(1, myDF.shape[0]+1)) # +1 to account for header row
 
     # create empty dataframe to work with.
-    tempDF = pd.DataFrame(columns=list(df.columns.values), index=np.arange(2))
+    tempDF = pd.DataFrame(columns=list(myDF.columns.values), index=np.arange(2))
 
     # iterate through the strings
     for ind, val in enumerate(searchList):
-        x = df[df[searchColName].str.contains(val, case=False) == True]
-        categoryColumn = [catList[ind]] * x.shape[0]
-        x.loc[:, newColName] = categoryColumn
-        tempDF = tempDF.append(x)
+        x = myDF[myDF[searchColName].str.match(val, case=False, na=False)]
+        y = x.copy()
+        y.loc[:, newColName] = catList[ind]
+        tempDF = tempDF.append(y)
 
     # remove NAs that were induced when empty dataframe was created
     clean = tempDF.dropna(axis=0, how='all')
 
     # merge the orginal and newly categorized dataframes
-    final = df.append(clean)
+    final = myDF.append(clean)
 
-    retry = final[final[newColName] == undefined]
+    retry = final[final[newColName] == undefinedValue]
 
     invertedSearchList = [typeChanger(x) for x in searchList]
 
     for ind, val in enumerate(invertedSearchList):
-        x = retry[retry[searchColName].str.contains(val, case=False) == True]
-        final.set_value(x.index, newColName, catList[ind])
+        x = retry[retry[searchColName].str.contains(val, case=False, regex=False) == True]
+        final.at[x.index, newColName] = catList[ind]
 
     # remove duplicate rows
     final = final.drop_duplicates(subset='ID', keep='last')
@@ -67,5 +60,3 @@ def categorizeDF(df, searchColName, searchList, catList, newColName="Category", 
     final = final.sort_values(by='ID')
     final = final.drop('ID', axis=1)
     return final
-
-catTest = categorizeDF(food, 'dish', search, category)
